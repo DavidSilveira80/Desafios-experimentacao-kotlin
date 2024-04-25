2fd56432d35738c887d96261e3306959dfd1a952
DavidSilveira80 <davidpscontato@gmail.com>
2024-04-25 16:04:41 -0300

    Reconfigurando.

---
+++ src/main/kotlin/iniciante/1000HeloWorld.kt
@@ -0,0 +1,5 @@
+package iniciante
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
---
+++ src/main/kotlin/iniciante/1001ExtremamenteBasico.kt
@@ -0,0 +1,12 @@
+package iniciante
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val entrada =  Scanner(System.`in`);
+    val A = entrada.nextInt()
+    val B = entrada.nextInt()
+
+    val soma:Int = A + B
+    println("X = " + soma)
+}
